1dca5d37e51297d2ef2c07dd1874d399a5a38772
bucksmaker11113-lang <bucksmaker11113@gmail.com>
2025-11-26 07:25:59 +0100

    Delete backend2 directory

--- backend2/routers/live.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-from services.tip_engine import generate_live_tips
-
-router = APIRouter(prefix="/tips")
-
-@router.get("/live")
-def get_live():
-    return generate_live_tips()
--- backend2/routers/tips.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-from services.tip_engine import generate_single_tips
-
-router = APIRouter(prefix="/tips")
-
-@router.get("/single")
-def get_single():
-    return generate_single_tips()
